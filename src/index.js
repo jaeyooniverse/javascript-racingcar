7fbc76777cb52e6741836d0d8e8198ca372e09ac
jaeyooniverse <jaeyoon4819@naver.com>
2022-04-18 12:47:31 +0900

    예외 처리, 주석 추가

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ carNamesInput.addEventListener('input', handleInputCar);
 
 const checkInputCar = (array) => {
   for (let i = 0; i < array.length; i++) {
-    if (array[i].length > 5) {
+    // 자동차 이름이 5자 이상을 넘어가거나 공백이면 재입력
+    if (array[i].length > 5 || array[i].length === 0) {
       return false;
     }
   }
@@ -25,9 +26,10 @@ const handleClickCar = (event) => {
   cars = inputCar.split(',');
 
   if (checkInputCar(cars)) {
-    racingCountInput.focus();
+    racingCountInput.focus(); // 자동차 이름을 올바르게 입력했으면 focus를 racing-count-input으로 변경
   } else {
     alert('입력창을 확인해주세요!');
+    cars = [];
   }
 };
 document
@@ -39,26 +41,28 @@ let inputCount = 0;
 let racingCountInput = document.querySelector('#racing-count-input');
 
 const handleInputCount = (event) => {
-  inputCount = event.target.value; // html input type이 number이므로 문자열 입력은 되지 않을뿐더러, 문자가 아닌 숫자로 저장된다.
+  inputCount = event.target.value; // html input type이 number이므로 문자열 입력은 되지 않을뿐더러, 문자가 아닌 숫자로 저장
 };
 racingCountInput.addEventListener('input', handleInputCount);
 
 let carsCount = [];
 
-// count 저장 받았으니, count만큼 시도하는 함수
-// count만큼 배열 요소 하나 하나 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 전진, 아니면 그냥 가만히
-
 let logs = document.querySelector('#logs');
 
 const play = (array) => {
+  /*
+   * 전진 조건은 0부터 9사이에서 무작위 값을 구한 후 무작위 값이 4 이상인 경우 전진
+   * 자동차 전진 횟수를 배열로 받아 전진하면 횟수를 1 증가
+   */
   for (let i = 0; i < array.length; i++) {
     if (Math.floor(Math.random() * 10) >= 4) {
       array[i] += 1;
     }
   }
 
+  // 매 게임 실행 결과를 출력해야 하므로 반복문을 통해 실행 결과를 출력
   for (let i = 0; i < array.length; i++) {
-    logs.innerHTML += `${cars[i]}:${'-'.repeat(array[i])}<br/>`;
+    logs.innerHTML += `${cars[i]}:${'-'.repeat(array[i])}<br/>`; // repeat()을 통해 '-'를 여러번 표현
   }
 };
 
@@ -68,6 +72,7 @@ const findWinner = (array) => {
   winner.innerHTML = '';
   let winnerCount = 0;
 
+  // 자동차 전진 횟수 배열에서 우승한 자동차의 전진 횟수를 저장
   for (let i = 0; i < array.length; i++) {
     if (array[i] >= winnerCount) {
       winnerCount = array[i];
@@ -76,6 +81,7 @@ const findWinner = (array) => {
 
   const winners = [];
 
+  // 공동 우승이 있을 수 있으므로 가장 많이 전진한 자동차들을 우승자들 배열에 저장
   for (let i = 0; i < array.length; i++) {
     if (array[i] === winnerCount) {
       winners.push(cars[i]);
@@ -86,6 +92,7 @@ const findWinner = (array) => {
   winner.innerHTML = `${winnersString}`;
 };
 
+// 결과를 출력하고 모든 정보를 초기화하는 함수
 const reset = () => {
   inputCar = '';
   cars = [];
@@ -101,10 +108,12 @@ const handleClickCount = (event) => {
 
   logs.innerHTML = '';
 
+  // 자동차 수만큼 자동차들의 전진 횟수를 저장할 배열에 0을 넣어준다.
   for (let i = 0; i < cars.length; i++) {
     carsCount.push(0);
   }
 
+  // 입력한 시도 횟수만큼 게임을 실행
   for (let i = 0; i < inputCount; i++) {
     play(carsCount);
     logs.innerHTML += `<br/>`;
